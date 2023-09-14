692c571da206fe80f305bec39803a3ad92c49b59
quannguyenhai2001 <nguyenhaiquan.0121@gmail.com>
2023-09-14 17:19:26 +0700

    code base screens

--- src/routes/routes.config.jsx
+++ src/routes/routes.config.jsx
@@ -1,13 +1,28 @@
 
-import HomeScreen from "features/Home/HomeScreen/HomeScreen";
+
 import DefaultLayout from "layouts/DefaultLayout/DefaultLayout";
+import HomeScreen from "screens/HomeScreen/HomeScreen";
+import LoginScreen from "screens/LoginScreen/LoginScreen";
+import OneCardFeatureScreen from "screens/OneCardFeatureScreen/OneCardFeatureScreen";
 
 
 export const RouteConfigs = [
 
     {
+        path: "/",
+        element: LoginScreen,
+        isPrivate: false,
+        layout: DefaultLayout,
+    },
+    {
         path: "/dashboard",
         element: HomeScreen,
+        isPrivate: false,
+        layout: DefaultLayout,
+    },
+    {
+        path: "/fb-one-card-picture-carousel",
+        element: OneCardFeatureScreen,
         isPrivate: false,
         layout: DefaultLayout,
     },
---
+++ src/screens/OneCardFeatureScreen/OneCardFeatureScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OneCardFeatureScreen = () => {
+    return (
+        <div>
+            features
+        </div>
+    );
+};
+
+export default OneCardFeatureScreen;
