8ad62ca997a5aabfb42644d326252153c78f5b86
Joamcriff <tan.nguyen3@monstar-lab.com>
2023-09-16 23:43:08 +0700

    test

--- src/screens/LoginScreen/LoginScreen.jsx
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import FacebookLogin from 'react-facebook-login';
 import {LoginSocialFacebook} from 'reactjs-social-login'
 import { FacebookLoginButton } from 'react-social-login-buttons';
+import axios from 'axios';
 
 
 const LoginScreen = () => {
+
+    useEffect(() => {
+        const fetch = async () => {
+            axios.get(`https://graph.facebook.com/2060605517611588?
+            fields=access_token&
+            access_token=EAAJx8mwbz58BO5ihfG36QxAqudkdVIzZBsMZCliRfleA6IHQd7NJBqVvZA2VZB1bsPFYO9bZAChBZBZBNeqjMBmZA06MBLZCXO4AGTZBJiAkeFCZAFaUZBn5EIamjcuQyMFfISST9eP1xtV20ZC5H4O0a7DuQ7gqedR4STkWJNpKLXIaz1Elri6ZBGjNRXXvVJmzyjyjR5cecfbkBv0RhR8VM72l1zHnkl0D0ZBLJS5QnoXQ6qIZBM0ZD`)
+                .then(response => {
+                    console.log('Bài viết đã được đăng:', response.data);
+                })
+                .catch(error => {
+                    console.error('Lỗi khi đăng bài')
+                })
+
+        }
+        fetch()
+    }, [])
+
     return (
         <div>
             <LoginSocialFacebook
